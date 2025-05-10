6a7e7a4bbeed4292168ca05d98a5be6ebbe2c33d
Betsaidars <betsaidars97@gmail.com>
2025-05-10 16:42:09 +0100

    Boton de agregar descuento y quitar

--- app/src/main/java/com/example/megatech/Views/FormularioCompraView.kt
+++ app/src/main/java/com/example/megatech/Views/FormularioCompraView.kt
@@ -1,13 +1,13 @@
-// FormularioCompraView.kt
-
 package com.example.megatech.Views
 
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
@@ -16,21 +16,22 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.example.megatech.Model.ItemsModel
 import com.example.megatech.SessionManager
 import com.example.megatech.ViewModels.CarritoDeCompraViewModel
-import com.example.megatech.ViewModels.ListaDeDeseosViewModel
 import com.example.megatech.ViewModels.PedidoViewModel
-import com.example.megatech.ViewModels.PedidoViewModelFactory
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormularioCompraView(navController: NavController, carritoDeCompraViewModel: CarritoDeCompraViewModel, sessionManager: SessionManager,  pedidoViewModel: PedidoViewModel) {
+fun FormularioCompraView(
+    navController: NavController,
+    carritoDeCompraViewModel: CarritoDeCompraViewModel,
+    sessionManager: SessionManager,
+    pedidoViewModel: PedidoViewModel
+) {
     val carritoListItems by carritoDeCompraViewModel.itemCarrito.collectAsState()
     val totalPrecioSeleccionado by carritoDeCompraViewModel.totalPrecioSeleccionado.collectAsState(initial = 0.0)
     var nombre by remember { mutableStateOf("") }
@@ -40,10 +41,10 @@ fun FormularioCompraView(navController: NavController, carritoDeCompraViewModel:
     val paymentMethods = listOf("Tarjeta de Crédito", "Bizum", "PayPal")
     var expanded by remember { mutableStateOf(false) }
     var selectedPaymentMethod by remember { mutableStateOf("Método de Pago") }
-
     var codigoDescuento by remember { mutableStateOf("") }
     val descuentoAplicado = remember { mutableStateOf(0.0) }
-    val descuentoAplicadoCodigo = remember { mutableStateOf("") } // Para guardar el código aplicado
+    val descuentoAplicadoCodigo = remember { mutableStateOf("") }
+    val mainScrollState = rememberScrollState() // Estado para el scroll principal
 
     Scaffold(
         topBar = {
@@ -83,7 +84,7 @@ fun FormularioCompraView(navController: NavController, carritoDeCompraViewModel:
                             nombreUsuario = nombre,
                             direccionEnvio = direccion,
                             items = carritoListItems.mapNotNull { it.id },
-                            total =  precioConDescuento.coerceAtLeast(0.0),
+                            total = precioConDescuento.coerceAtLeast(0.0),
                             metodoPago = selectedPaymentMethod,
                             codigoDescuento = descuentoAplicadoCodigo.value.ifBlank { null }
                         )
@@ -97,23 +98,28 @@ fun FormularioCompraView(navController: NavController, carritoDeCompraViewModel:
                         }
                     },
                     enabled = nombre.isNotBlank() && direccion.isNotBlank() && carritoListItems.isNotEmpty() && selectedPaymentMethod.isNotBlank(),
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(4.dp)
+
                 ) {
                     Text("Pagar")
                 }
             }
         }
     ) { paddingValues ->
-        Column(
+        Column( // Volvemos a usar Column como contenedor principal
             modifier = Modifier
                 .padding(paddingValues)
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(horizontal = 16.dp)
+                .verticalScroll(mainScrollState), // Aplicamos verticalScroll al Column principal
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.height(8.dp))
-            LazyColumn {
-                items(carritoListItems, key = { it.id ?: "" }) { item ->
+            Column( // Usamos un Column normal para la lista de ítems
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                carritoListItems.forEach { item -> // Iteramos sobre la lista directamente
                     FormularioItemRow(
                         item = item,
                         onRemoveItem = { itemToRemove ->
@@ -151,15 +157,13 @@ fun FormularioCompraView(navController: NavController, carritoDeCompraViewModel:
                 modifier = Modifier.fillMaxWidth()
             )
 
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
             // Desplegable para el método de pago
             ExposedDropdownMenuBox(
                 expanded = expanded,
                 onExpandedChange = { expanded = !expanded },
-                modifier = Modifier
-                    .fillMaxWidth()
-
+                modifier = Modifier.fillMaxWidth()
             ) {
                 OutlinedTextField(
                     value = selectedPaymentMethod,
@@ -206,7 +210,7 @@ fun FormularioCompraView(navController: NavController, carritoDeCompraViewModel:
                     value = codigoDescuento,
                     onValueChange = { codigoDescuento = it },
                     label = { Text("Código Descuento") },
-                    modifier = Modifier.weight(0.7f) // Ocupa el 70% del ancho
+                    modifier = Modifier.weight(0.7f)
                 )
                 Spacer(modifier = Modifier.width(8.dp))
                 Button(
@@ -223,12 +227,12 @@ fun FormularioCompraView(navController: NavController, carritoDeCompraViewModel:
                         } else {
                             descuentoAplicado.value = 0.0
                             descuentoAplicadoCodigo.value = ""
-                            codigoDescuento = "" // Limpiar el TextField al quitar
+                            codigoDescuento = ""
                         }
                     },
-                    modifier = Modifier
-                        .weight(0.3f) // Ocupa el 30% del ancho
-                        .
+                    modifier = Modifier.weight(0.3f),
+                    shape = RoundedCornerShape(4.dp)
+
                 ) {
                     Text(if (descuentoAplicadoCodigo.value == codigoDescuento && descuentoAplicadoCodigo.value.isNotBlank()) "Quitar" else "Aplicar")
                 }
@@ -238,13 +242,15 @@ fun FormularioCompraView(navController: NavController, carritoDeCompraViewModel:
                     text = "Descuento aplicado: ${String.format("%.2f", descuentoAplicado.value)}€",
                     style = MaterialTheme.typography.bodySmall,
                     color = Color.Green,
-                    modifier = Modifier.padding(top = 4.dp).align(Alignment.CenterHorizontally)
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .align(Alignment.CenterHorizontally)
                 )
             }
+            Spacer(modifier = Modifier.height(16.dp)) // Añade un espacio al final del contenido
         }
     }
 }
-
 
 @Composable
 fun FormularioItemRow(item: ItemsModel, onRemoveItem: (ItemsModel) -> Unit) {
