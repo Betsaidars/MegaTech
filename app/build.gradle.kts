a544d446ace012769481fcbaf652f4776387a0ea
Betsaidars <betsaidars97@gmail.com>
2025-05-19 21:15:30 +0100

    logo app hecho

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.gms.google-services")
+    id("org.jetbrains.dokka") version "1.9.10"
 }
 
 android {
